69fc3ad5de21266e24ef83394f908033317f407f
Jubayer Al Mamun <jubayeramb@gmail.com>
2025-02-12 15:59:35 +0600

    feat: add verification status to reports

---
+++ prisma/migrations/20250212094825_/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `full_name` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "reports" ADD COLUMN     "verified" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "full_name" TEXT NOT NULL;
