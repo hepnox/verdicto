fb445767ead32ccc5780bb5f420cd0823a86ce11
Jubayer Al Mamun <jubayeramb@gmail.com>
2025-02-12 11:50:29 +0600

    feat: update creatre post link

--- components/create-post-button.tsx
+++ components/create-post-button.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function CreatePostButton() {
   return (
     <Button asChild variant="default" size="sm">
-      <Link href="/create-post" className="flex items-center gap-2">
+      <Link href="/post" className="flex items-center gap-2">
         <Plus className="h-4 w-4" />
         <span>Create Post</span>
       </Link>
