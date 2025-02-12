3ac9b1bc8a1aab7b4e8794cdaeed25005a8759e8
zahinafsar <afsarzahin@gmail.com>
2025-02-12 16:51:44 +0600

    fix: toast notification

--- components/notification.ts
+++ components/notification.ts
@@ -6,17 +6,19 @@ import { messaging } from '@/lib/firebase/client';
 import useFcmToken from '@/hooks/use-fcm';
 import { createClient } from '@/lib/supabase/client';
 import { useAuth } from '@/hooks/use-auth';
+import { useToast } from '@/hooks/use-toast';
 
 const supabase = createClient();
 
 export default function SubscribeNotification() {
+    const { toast } = useToast();
     const { user } = useAuth();
     const { fcmToken } = useFcmToken();
 
     useEffect(() => {
         const updateFcmToken = async () => {
             if (fcmToken && user?.id) {
-               await supabase.from('users').update({ fcm_token: fcmToken }).eq('id', user?.id);
+                await supabase.from('users').update({ fcm_token: fcmToken }).eq('id', user?.id);
             }
         };
         updateFcmToken();
@@ -25,7 +27,12 @@ export default function SubscribeNotification() {
     useEffect(() => {
         if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
             const unsubscribe = onMessage(messaging, (payload) => {
-                console.log('Foreground push notification received:', payload);
+                if (payload.notification) {
+                    toast({
+                        title: payload.notification.title,
+                        description: payload.notification.body,
+                    })
+                }
             });
             return () => unsubscribe();
         }
